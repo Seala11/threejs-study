776255d63d32f117232952dc5794280cb47acbcd
Hanna Papova <seal.annuska@gmail.com>
2023-01-15 14:21:33 -0300

    feat: add orbit controls

--- src/entry.js
+++ src/entry.js
@@ -8,6 +8,7 @@ import {
   Vector2,
   PointLight,
 } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import DonutScene from "./objects/Scene.js";
 
 const scene = new Scene();
@@ -31,9 +32,12 @@ scene.add(pointLight);
 
 const plane = new Plane(new Vector3(0, 0, 1), 1);
 const raycaster = new Raycaster();
-const pointOfIntersection = new Vector3();
 
-function onPointerMove(event) {
+const target = new Vector3();
+let mouseX = 0;
+let mouseY = 0;
+
+const onPointerMove = (event) => {
   pointer.x = event.clientX;
   pointer.y = event.clientY;
 
@@ -43,12 +47,20 @@ function onPointerMove(event) {
   pointLight.position.y = mouse.y;
   pointLight.position.x = mouse.x;
 
-  raycaster.setFromCamera(mouse, camera);
-  raycaster.ray.intersectPlane(plane, pointOfIntersection);
-  donutScene.lookAt(pointOfIntersection);
-}
+  mouseX = event.clientX - window.innerWidth / 2;
+  mouseY = event.clientY - window.innerHeight / 2;
 
-function onTouchMove(event) {
+  target.x += (mouseX - target.x) * 0.02;
+  target.y += (-mouseY - target.y) * 0.02;
+  target.z = camera.position.z;
+};
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+let touchX = 0;
+let touchY = 0;
+const onTouchMove = (event) => {
   pointer.x = event.changedTouches[0].clientX;
   pointer.y = event.changedTouches[0].clientY;
 
@@ -58,10 +70,13 @@ function onTouchMove(event) {
   pointLight.position.y = mouse.y;
   pointLight.position.x = mouse.x;
 
-  raycaster.setFromCamera(mouse, camera);
-  raycaster.ray.intersectPlane(plane, pointOfIntersection);
-  donutScene.lookAt(pointOfIntersection);
-}
+  touchX = event.clientX - window.innerWidth / 2;
+  touchY = event.clientY - window.innerHeight / 2;
+
+  target.x += (touchX - target.x) * 0.02;
+  target.y += (-touchY - target.y) * 0.02;
+  target.z = camera.position.z;
+};
 
 window.addEventListener("mousemove", onPointerMove);
 window.addEventListener("touchmove", onTouchMove);
@@ -73,6 +88,7 @@ renderer.setClearColor(0xa567cf, 0.5);
 // render loop
 const onAnimationFrameHandler = (timeStamp) => {
   donutScene.update(timeStamp);
+
   renderer.render(scene, camera);
   window.requestAnimationFrame(onAnimationFrameHandler);
 };
